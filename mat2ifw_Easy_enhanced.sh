c1d09806ad89a8ab11df990b34282d6af7b43d84
zuwangzang <zuwangzang@gmail.com>
2018-01-30 16:04:02 +0800

    optimize the code of mat2ifw_Easy_enhanced.sh

--- mat2ifw_Easy_enhanced.sh
+++ mat2ifw_Easy_enhanced.sh
@@ -14,37 +14,24 @@ declare -a array
 
 a=`cat $filename | awk -F "/" '{print $1}' | uniq`
 array=($a)
-
-function ct(){
-temfile=$1
-
-echo "<rules> \
-
-	 <broadcast block=\"true\" log=\"false\"> \
-
-	  </broadcast> \
-
-	   <service block=\"true\" log=\"false\"> \
-
-	    </service> \
-
-	     <activity block=\"true\" log=\"false\"> \
-
-	      </activity> \
-
-	      </rules>" > $temfile
-}
+srclist=$(cat $filename | sed '/./{s/^/  <component-filter name="&/g;s/$/&\"\ \/>/g}')
 
 for i in ${a[*]}; do
 
-file1="$i.xml"
-file2="$i.tp2"
-ct ./ifw/$file1
-
-cat $filename | grep $i | sed '/./{s/^/  <component-filter name="&/g;s/$/&\"\ \/>/g}' > $file2
-echo $file2 | sed -i "/log/r $file2" ./ifw/$file1
-
-
-rm -rf $file2
+	file="$i.xml"
+	midlist=$(echo "${srclist}" | grep $i)
+	dstlist="<rules>
+<broadcast block=\"true\" log=\"false\">
+"${midlist}"
+</broadcast>
+<service block=\"true\" log=\"false\">
+"${midlist}"
+</service>
+<activity block=\"true\" log=\"false\">
+"${midlist}"
+</activity>
+</rules>"
+	echo "${dstlist}" > ./ifw/$file
 
 done
+
